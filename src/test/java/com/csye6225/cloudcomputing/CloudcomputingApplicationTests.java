3a72f48453198fb5c088df2e2623d99684d7694a
Dhaval Pandya <pandya.d@northeastern.edu>
2020-09-30 08:36:08 -0400

    add initail project structure and git action

---
+++ src/test/java/com/csye6225/cloudcomputing/CloudcomputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csye6225.cloudcomputing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcomputingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
