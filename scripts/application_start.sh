878dc34e458f5cfb6fa771c8eadd8762bf59ef62
Dhaval Pandya <pandya.d@northeastern.edu>
2020-11-02 23:29:12 -0500

    testing workflow

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/webapp
 
 cd ..
 
-sudo chmod uo+rwx webapp
-
-cd webapp/
-
-mvn package
-
-mv target/cloudcomputing-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/
+sudo mv target/cloudcomputing-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/
 
 cd /opt/tomcat/webapps/
 
