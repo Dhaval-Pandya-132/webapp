3f833cc6fe55e0800600bf5e873f4d48a6dc5067
Dhaval Pandya <pandya.d@northeastern.edu>
2020-11-12 08:15:38 -0500

    Assignment 7 changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,8 +12,12 @@ ls -al
 
 sudo mv target/webapp.war /opt/tomcat/webapps/
 
+sudo mv cloudWatchConfig.json /opt/aws/amazon-cloudwatch-agent/etc/
+
 cd /opt/tomcat/webapps/
 
 ls -al
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudWatchConfig.json -s
 
 sudo /opt/tomcat/bin/startup.sh
