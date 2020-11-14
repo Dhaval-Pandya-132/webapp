a6bfa189697edff3452ce331aaae30b0b0f3f7e6
Dhaval Pandya <pandya.d@northeastern.edu>
2020-11-14 15:03:58 -0500

    assignment7 demo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -16,6 +16,7 @@ sudo mv cloudWatchConfig.json /opt/aws/amazon-cloudwatch-agent/etc/
 
 cd /opt/tomcat/webapps/
 
+
 ls -al
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudWatchConfig.json -s
